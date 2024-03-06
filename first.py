d2fad5e3e212c2a3b30b280058de3e96df7f8599
PruthviChavan21 <chavan2012@gmail.com>
2024-03-06 23:36:58 +0530

    simple ec2 instance

--- first.py
+++ first.py
@@ -1,1 +1,16 @@
 import boto3
+
+# Initialize the EC2 resource
+ec2 = boto3.resource('ec2')
+
+# Create an EC2 instance
+instance = ec2.create_instances(
+    ImageId='ami-0f403e3180720dd7e',  # Specify the Amazon Machine Image (AMI) ID
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',  # Choose the instance type (e.g., t2.micro)
+)
+
+# Print the instance ID
+print(f"Instance ID: {instance[0].id}")
+#print(f"Instance ID: {instance[0].id}")

2a443a3295f38b567765e0da48a71b718060b16b
PruthviChavan21 <chavan2012@gmail.com>
2024-09-02 15:25:25 +0530

    if loop

---
+++ if.py
@@ -0,0 +1,13 @@
+if 6 > 3:
+    print("five is the best thing that is required")
+
+#    I have named myself that the named the king
+
+print("this is awesome")
+
+a = [1,2,3,0]
+
+if a[3] > 1:
+    print("Name is less")
+else:
+    print("this is npt roght")
